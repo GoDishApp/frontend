a84469064a7e42fb17279bb8b4ab0c6cf8311460
bchin224 <bchin224@gmail.com>
2021-03-24 11:37:55 -0400

    Add input group, form, buttons, slider - Import InputGroup, Button and FormControl from react-bootstrap - Import Typography and Slider from Material-UI

--- react-auth-template-master/src/components/HomePage/HomePage.js
+++ react-auth-template-master/src/components/HomePage/HomePage.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
+import FormControl from 'react-bootstrap/FormControl'
+import Typography from '@material-ui/core/Typography'
+import Slider from '@material-ui/core/Slider'
 
 class HomePage extends Component {
   constructor (props) {
@@ -12,7 +17,31 @@ class HomePage extends Component {
 
   render () {
     return (
-      <p>dogs</p>
+      <div className="homepage">
+        <p>Home Page Loaded</p>
+        <div>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <Button variant="outline-secondary">Button</Button>
+            </InputGroup.Prepend>
+            <FormControl aria-describedby="basic-addon1" />
+          </InputGroup>
+          <Button>Vegetarian?</Button>
+          <Button>Vegan?</Button>
+          <Typography id="discrete-slider-small-steps" gutterBottom>
+            Small steps
+          </Typography>
+          <Slider
+            defaultValue={10}
+            aria-labelledby="discrete-slider-small-steps"
+            step={1}
+            marks
+            min={0}
+            max={25}
+            valueLabelDisplay="auto"
+          />
+        </div>
+      </div>
     )
   }
 }
