acd333d22b26fb5a69b3215dec741842caf02287
bchin224 <bchin224@gmail.com>
2021-03-24 15:41:19 -0400

    Added test photos and carousel

--- react-auth-template-master/src/components/GetDish/GetDish.js
+++ react-auth-template-master/src/components/GetDish/GetDish.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react'
+import ReactSwipe from 'react-swipe'
+import Image from 'react-bootstrap/Image'
+import burger from './burger.jpg'
+import salad from './salad.jpg'
+import acai from './acai_bowl.jpg'
+import avocado from './avocado_toast.jpg'
+import noodles from './noodle_bowl.jpg'
+import pancakes from './pancakes.jpg'
+
+let reactSwipeEl
 
 class GetDish extends Component {
   constructor (props) {
@@ -18,6 +28,34 @@ class GetDish extends Component {
     return (
       <div className="dishes">
         <p>Compare dishes</p>
+        <div>
+          <ReactSwipe
+            className="carousel"
+            swipeOptions={{ continuous: false }}
+            ref={el => (reactSwipeEl = el)}
+          >
+            <div className="dish">
+              <Image src={burger} alt="Burger"/>
+            </div>
+            <div>
+              <Image src={salad} alt="Salad"/>
+            </div>
+            <div>
+              <Image src={acai} alt="Acai Bowl"/>
+            </div>
+            <div>
+              <Image src={avocado} alt="Avocado Toast"/>
+            </div>
+            <div>
+              <Image src={noodles} alt="Noodle Bowl"/>
+            </div>
+            <div>
+              <Image src={pancakes} alt="Pancakes"/>
+            </div>
+          </ReactSwipe>
+          <button onClick={() => reactSwipeEl.prev()}>Previous</button>
+          <button onClick={() => reactSwipeEl.next()}>Next</button>
+        </div>
       </div>
     )
   }
